15cf0c34d570a4955dedffefa5c6105bfa05e927
wub05 <wubishet05@gmail.com>
2026-04-08 17:47:49 +0300

    ready for deploy v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import COCTraining from "./components/COCTraining";
 import Footer from "./components/Footer";
-import Header from "./components/header";
+import Header from "./components/Header";
 import HomePage from "./components/Homepage";
 import Services from "./components/Services";
 import StartNow from "./components/StartNow.jsx";
